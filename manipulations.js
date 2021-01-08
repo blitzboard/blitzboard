ed1aca34eb12f49b61aac39be81016530ce3b444
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2021-01-08 11:43:23 +0900

    Enable edge addition

--- manipulations.js
+++ manipulations.js
@@ -1,3 +1,5 @@
+let numberOfLines = (text) => (text.match(/\n/g) || '').length + 1;
+
 let manipulationOptions =
     {
       addNode: (data, callback) => {
@@ -93,15 +95,17 @@ function inputProperties() {
 function saveNode(data, callback) {
   clearPopUp();
   callback(null); // manually add nodes
-  const propText = inputProperties().map((input) => `${input.name}: '${input.value.trim()}'`).join(',');
-  let query = `CREATE (a:${inputLabel()} {${propText}}) RETURN a`;
-  axios.get(domain + `query?q=${query}`).then((response) => {
-    const node = response.data.pg.nodes[0];
-    nodeDataSet.add(toVisNode(node, "", displayedNodeProps(), data));
-    graph.nodes.push(node);
-    nodeMap[node.id] = node;
-    q('#graph-input').value = json2pg.translate(JSON.stringify(graph));
-  });
+  if(!localMode) {
+    const propText = inputProperties().map((input) => `${input.name}: '${input.value.trim()}'`).join(',');
+    let query = `CREATE (a:${inputLabel()} {${propText}}) RETURN a`;
+    axios.get(domain + `query?q=${query}`).then((response) => {
+      const node = response.data.pg.nodes[0];
+      nodeDataSet.add(toVisNode(node, "", displayedNodeProps(), data));
+      graph.nodes.push(node);
+      nodeMap[node.id] = node;
+      q('#graph-input').value = json2pg.translate(JSON.stringify(graph));
+    });
+  }
 }
 
 function saveEdge(data) {
@@ -117,13 +121,24 @@ function saveEdge(data) {
   clearPopUp();
   const propText = inputProperties().map((input) => `${input.name}: '${input.value.trim()}'`).join(',');
   graph.edges.push(newEdge);
-  console.log(data);
   q('#graph-input').value = json2pg.translate(JSON.stringify(graph));
-  let query = `MATCH (a),(b) WHERE id(a) = ${data.from} and id(b) = ${data.to} CREATE (a)-[r2:${inputLabel()} {${propText}}]->(b) RETURN r2`;
-  axios.get(domain + `query?q=${query}`).then((response) => {
-    // TODO: add new edge to edgeMap
-    edgeDataSet.add(toVisEdge(response.data.pg.edges[0]))
-  });
+  if(!localMode) {
+    let query = `MATCH (a),(b) WHERE id(a) = ${data.from} and id(b) = ${data.to} CREATE (a)-[r2:${inputLabel()} {${propText}}]->(b) RETURN r2`;
+    axios.get(domain + `query?q=${query}`).then((response) => {
+      // TODO: add new edge to edgeMap
+      edgeDataSet.add(toVisEdge(response.data.pg.edges[0]))
+    });
+  } else {
+    let visEdge = toVisEdge(newEdge);
+    edgeMap[visEdge.id] = newEdge
+    edgeDataSet.add(visEdge);
+
+    let oldPg = editor.getValue();
+    newEdge.line = numberOfLines(oldPg);
+    byProgram = true;
+    editor.setValue(oldPg + `\n"${newEdge.from}" -- "${newEdge.to}" ${newEdge.labels.map((label) => ':' + label).join(' ')} `);
+    scrollToLine(newEdge.line);
+  }
 }
 
 
@@ -135,31 +150,37 @@ function editNode(data, callback) {
     updateText += ', ' + propInputs.map((input) => `a.${input.name} = '${input.value.trim()}'`).join(',');
   clearPopUp();
   callback(null); // edit node without vis.js default behavior
-  let query = `MATCH (a) WHERE id(a) = ${data.id} SET ${updateText} RETURN a`;
-  axios.get(domain + `query?q=${query}`).then((response) => {
-    // TODO: edit label in PG
-    // TODO: support removal of existing pg
-    nodeDataSet.update(toVisNode(response.data.pg.nodes[0], "", displayedNodeProps(), data));
-    // graph.nodes.push(node);
-    // q('#graph-input').value = json2pg.translate(JSON.stringify(graph));
-  });
+  if(!localMode) {
+    let query = `MATCH (a) WHERE id(a) = ${data.id} SET ${updateText} RETURN a`;
+    axios.get(domain + `query?q=${query}`).then((response) => {
+      // TODO: edit label in PG
+      // TODO: support removal of existing pg
+      nodeDataSet.update(toVisNode(response.data.pg.nodes[0], "", displayedNodeProps(), data));
+      // graph.nodes.push(node);
+      // q('#graph-input').value = json2pg.translate(JSON.stringify(graph));
+    });
+  }
 }
 
 function deleteNode(data) {
-  let query = `MATCH (n) WHERE id(n) IN [${data.nodes.join(',')}] DETACH DELETE n`;
-  axios.get(domain + `query?q=${query}`).then((response) => {
-    console.log(response);
-  });
+  if(!localMode) {
+    let query = `MATCH (n) WHERE id(n) IN [${data.nodes.join(',')}] DETACH DELETE n`;
+    axios.get(domain + `query?q=${query}`).then((response) => {
+      console.log(response);
+    });
+  }
 }
 
 
 function deleteEdge(data) {
   const edgeId = edgeDataSet.get(data.edges[0]).remoteId;
-  let query = `MATCH (n)-[e]-(n2) WHERE id(e) = ${edgeId} DELETE e`;
-  console.log(query);
-  axios.get(domain + `query?q=${query}`).then((response) => {
-    console.log(response);
-  });
+  if(!localMode) {
+    let query = `MATCH (n)-[e]-(n2) WHERE id(e) = ${edgeId} DELETE e`;
+    console.log(query);
+    axios.get(domain + `query?q=${query}`).then((response) => {
+      console.log(response);
+    });
+  }
 }
 
 
