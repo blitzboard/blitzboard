93b465855f5334179f7ccbf101d3f469f1ca17f2
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2021-05-07 16:41:42 +0900

    Extract config for visualization to config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+let config = {
+  node: {
+    icon: {
+      programmer: 'f3a0',
+      designer: 'f37b',
+      location: 'f2e9',
+      hackathon: 'f380'
+    }
+  },
+  layout: 'hierarchical',
+  //layout: 'custom',
+  layoutMapping: {
+    x: 'x',
+    y: 'y'
+  }            
+};
